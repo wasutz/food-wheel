032b3a3a8fa03ab69e58728d505bd8c5158b0cfa
Wasut <wasut_pao@hotmail.com>
2026-03-04 00:01:15 +0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang={appConfig.app.defaultLanguage}>
       <head>
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" href="/icons/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
